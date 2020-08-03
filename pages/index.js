6efd1f137d74f1bc936c11f79c6072fc7bfc1567
Bryan Rogel <bryanrogeldev@gmail.com>
2020-08-03 16:19:43 -0600

    Header:
    Create a header layout.

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
-export default function Home() {
+import Header from './components/header/Header';
+
+function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>NEWS!!!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Header/>
       <main className={styles.main}>
         <h1 className={styles.title}>
           Usando <a href="https://nextjs.org">Next.js!</a>
@@ -17,3 +19,5 @@ export default function Home() {
     </div>
   )
 }
+
+export default Home;
