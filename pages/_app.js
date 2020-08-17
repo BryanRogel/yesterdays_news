af817a189bf04cc3395ade1c136ba22085dd47f6
Bryan Rogel <bryanrogeldev@gmail.com>
2020-08-16 23:08:38 -0600

    refactor(Carousel):
    Change the carousel component.

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
-import '../styles/globals.css';
-import '../styles/bootstrap.min.css';
 import { Provider } from 'react-redux';
 
 import store from '../store';
+
+import '../styles/globals.css';
+import '../styles/styleCarousel.css';
+import '../styles/bootstrap.min.css';
 
 export default function MyApp({ Component, pageProps }) {
   return (
